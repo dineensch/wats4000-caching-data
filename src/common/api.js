059e9e405c3b8f53ef91e10bab3fc4150db6c257
Dineen <35275339+dineensch@users.noreply.github.com>
2018-08-14 17:04:11 -0700

    first push

--- src/common/api.js
+++ src/common/api.js
@@ -8,9 +8,8 @@ export const API = axios.create({
 
 API.interceptors.request.use(function (config) {
     // Set common parameters for each request to the API.
-
-    // TODO: Use your own APPID for OpenWeatherMap.org
-    config.params.APPID = 'YOUR_APPID_HERE';
+    
+    config.params.APPID = '192ade269df16bdea841c0f76b264815';
     config.params.units = 'imperial';
 
     return config;
